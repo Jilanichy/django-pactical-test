47b18428d1200802f32c574d8ee31223b4ddbc70
Ak Zilani <jilanichy57@gmail.com>
2022-08-18 12:45:31 +0600

    Relocate the customer app to the project directory

---
+++ customers/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import date
+from phonenumber_field.modelfields import PhoneNumberField
+
+# app contains three distinct plans
+PLAN_CHOICES = (
+    ('Bronze', 'Globalnet Bronze'),
+    ('Silver', 'Globalnet Silver'),
+    ('Gold', 'Globalnet Gold')
+)
+
+# making a few bangladeshi company for customer to choose from
+COMPANY_CHOICES = (
+    ('Robi', 'Robi'),
+    ('Airtel', 'Airtel'),
+    ('Grameenphone', 'Grameenphone'),
+    ('Teletalk', 'Teletalk')
+)
+
+class SubscriptionInfo(models.Model):
+    plan_type = models.CharField(choices=PLAN_CHOICES, max_length=6, blank=True)
+    admin = models.ForeignKey('auth.User', related_name='SubscriptionInfo', on_delete=models.CASCADE)
+    subscriber_name = models.CharField(max_length=100, blank=True)
+    primary_phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    other_phone_number = PhoneNumberField(blank=True)
+    subscribed_company = models.CharField(choices=COMPANY_CHOICES, max_length=14)
+    contract_initiated = models.DateTimeField(auto_now_add=True)
+    phone_number_owner = models.CharField(max_length=100, blank=True)
+    subscription_price = models.FloatField(blank=True, default=0)
+
+    # To get the price per plan based on customer plan selection, there's three plan in PLAN_CHOICES
+    def save(self, *args, **kwargs):
+        if self.plan_type == 'Bronze':
+            self.subscription_price = 500
+        elif self.plan_type == 'Silver':
+            self.subscription_price = 750
+        elif self.plan_type == 'Gold':
+            self.subscription_price = 1500
+        super(SubscriptionInfo, self).save(*args, **kwargs)
+
+    # if a subscriber subscribed to a paid plan, subscriber will own the phone number otherwise the company
+    def save(self, *args, **kwargs):
+        if self.plan_type:
+            self.phone_number_owner = self.subscriber
+        else:
+            self.phone_number_owner = self.subscribed_company
+
+        super(SubscriptionInfo, self).save(*args, **kwargs)
+
+
+    # To get a customer's contract remaining days
+    def get_contract_remaining_day(self):
+        if PLAN_CHOICES[2][0]:
+            return "No termination period, you can cancel any time"
+        else:
+            today = date.today()
+            remaining_day = self.contract_initiated.date() - today
+            remaining_day_stripped = str(remaining_day).split(",", 1)[0]
+            return f"Contact will end after {remaining_day_stripped} days"
+
+
+    # ordering models data based on contract initiated date of a customer
+    class Meta:
+        ordering = ['contract_initiated']
---
+++ customers/serializers.py
@@ -0,0 +1,31 @@
+from random import choices
+from rest_framework import serializers
+from customers.models import SubscriptionInfo, PLAN_CHOICES, COMPANY_CHOICES
+from django.contrib.auth.models import User
+
+
+class SubscriptionInfoSerializer(serializers.ModelSerializer):
+    subscriber = serializers.ReadOnlyField(source='subscriber.username')
+
+    class Meta:
+        model = SubscriptionInfo
+        fields = ['id', 'subscriber', 'primary_phone_number', 'other_phone_number', 
+            'plan_type', 'subscription_price', 'subscribed_company', 'phone_number_owner', 
+            'contract_initiated']
+ 
+
+    def create(self, validated_data):
+        """
+        Create and return a new `SubscriptionInfo` instance, given the validated data.
+        """
+        return SubscriptionInfo.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `SubscriptionInfo` instance, given the validated data.
+        """
+        instance.plan_type = validated_data.get('plan_type', instance.plan_type)
+        instance.other_phone_number = validated_data.get('other_phone_number', instance.other_phone_number)
+        instance.subscribed_company = validated_data.get('subscribed_company', instance.subscribed_company)
+        instance.save()
+        return instance
